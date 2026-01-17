2e9d2b0b01e56ebac942470efec409d732c68c28
Mohamed-n-Samir <mohamed.n.work12@gmail.com>
2026-01-17 23:33:51 +0200

    modifiy some service methods

--- models/whatsapp_service.py
+++ models/whatsapp_service.py
@@ -1,21 +1,50 @@
 from odoo import models, api
+from odoo.exceptions import ValidationError
 
+import re
 
 
 class WhatsAppService(models.AbstractModel):
-    
-    _name = 'whatsapp.service'
-    _description = 'WhatsApp API Service'
-    
-    
+
+    _name = "whatsapp.service"
+    _description = "WhatsApp API Service"
+
     # Helper Methods
-    def _correct_phone_number_structure(self):
-        pass
-    
+    def _get_coutry_calling_code(self, country_code):
+
+        if not country_code:
+            return None
+
+        country = self.env["res.country"].search(
+            [("code", "=", country_code.upper())], limit=1
+        )
+
+        return country.phone_code if country else None
+
     def _check_connection(self):
         pass
-    
-    
+
     # Methods
-    def send_whatsapp_message(self):
+    def normalize_phone_e164(self, phone, country=None):
+
+        if not phone:
+            raise ValidationError("Phone Number is required!")
+
+        digits = re.sub(r"[^\d]", "", phone)
+
+        if phone.strip().startswith("+"):
+            return "+" + digits
+
+        if not country or not country.phone_code:
+            raise ValidationError("Country is required to normalize phone number")
+        
+        if digits.startswith('0'):
+            digits = digits[1:]
+            
+        normalized = f"+{country.phone_code}{digits}"
+        
+        if not (7 <= len(re.sub(r'[^\d]', '', normalized)) <= 15):
+            raise ValidationError("Invalid phone number")
+
+    def send_message(self):
         pass
